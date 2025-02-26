61fed413429b5c5fbfb9d14fe01aebeee647d9b6
Asura369 <linkevin1220@gmail.com>
2025-02-25 21:01:28 -0500

    Basic features; no result

---
+++ flip.js
@@ -0,0 +1,99 @@
+
+var yin = 2
+var yang = 3
+
+
+function randomNum() {
+    num1 = Math.floor(Math.random() * (2)) + 2
+    setCoin('coin1', 'text1', num1)
+    num2 = Math.floor(Math.random() * (2)) + 2
+    setCoin('coin2', 'text2', num2)
+    num3 = Math.floor(Math.random() * (2)) + 2
+    setCoin('coin3', 'text3', num3)
+    return num1 + num2 + num3
+}
+
+function setCoin(coinNum, coinText, num) {
+    if (num == 2) {
+        document.getElementById(coinNum).setAttribute('src', './images/head.jpeg')
+        document.getElementById(coinText).innerHTML = 'Yin'
+    }
+    else {
+        document.getElementById(coinNum).setAttribute('src', './images/tail.jpeg')
+        document.getElementById(coinText).innerHTML = 'Yang'
+    }
+}
+
+function setMark(mark) {
+    total = randomNum()
+    if (total == 6 || total == 8) {
+        document.getElementById(mark).setAttribute('src', './images/yin.png')
+        document.getElementById(mark).setAttribute('name', 'yin')
+    }
+    else {
+        document.getElementById(mark).setAttribute('src', './images/yang.png')
+        document.getElementById(mark).setAttribute('name', 'yang')
+    }
+}
+
+function getFullMark() {
+    one = document.getElementById('one').getAttribute('name')
+    two = document.getElementById('two').getAttribute('name')
+    three = document.getElementById('three').getAttribute('name')
+    four = document.getElementById('four').getAttribute('name')
+    five = document.getElementById('five').getAttribute('name')
+    six = document.getElementById('six').getAttribute('name')
+
+    result = []
+    result.push(one, two, three, four, five, six)
+    return result
+}
+
+function showResult() {
+    getFullMark()
+
+
+
+}
+
+function flipcoin() {
+
+    one = document.getElementById('one').getAttribute('src')
+    two = document.getElementById('two').getAttribute('src')
+    three = document.getElementById('three').getAttribute('src')
+    four = document.getElementById('four').getAttribute('src')
+    five = document.getElementById('five').getAttribute('src')
+    six = document.getElementById('six').getAttribute('src')
+
+    if (one == "") {
+        setMark("one")
+    }
+    else if (two == "") {
+        setMark("two")
+    }
+    else if (three == "") {
+        setMark("three")
+    }
+    else if (four == "") {
+        setMark("four")
+    }
+    else if (five == "") {
+        setMark("five")
+    }
+    else if (six == "") {
+        setMark("six")
+        document.getElementById('flip_button').innerHTML = 'Show Result'
+    }
+    else {
+        showResult()
+    }
+
+
+
+
+
+
+
+}
+
+
